f17b0db829c52bc5ecc710c2b38dd7e7bd123a99
chippee08 <paolobanag08@gmail.com>
2024-08-06 15:49:44 +0800

    xcdfd

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-const inputs = document.querySelectorAll("input");
+document.addEventListener("DOMContentLoaded", () => {
+    const inputs = document.querySelectorAll("input[type='text'], input[type='checkbox'], textarea");
 
-function submit(event) {
-    event.preventDefault(); // Prevent the form from submitting
-    inputs.forEach(input => {
-        if (input.value.trim() === "") {
-            input.style.border = "solid hsl(0, 66%, 54%) 1px";
-        } else {
-            input.style.border = "solid black 1px"; // Reset background color if input is valid
-        }
-    });
-}
+    function submit(event) {
+        event.preventDefault(); // Prevent the form from submitting
+        inputs.forEach(input => {
+            if (input.type === 'checkbox') {
+                if (!input.checked) {
+                    input.closest('div').style.border = "solid hsl(0, 66%, 54%) 1px";
+                } else {
+                    input.closest('div').style.border = "none"; // Reset border color if input is valid
+                }
+            } else {
+                if (input.value.trim() === "") {
+                    input.style.border = "solid hsl(0, 66%, 54%) 1px";
+                } else {
+                    input.style.border = "solid black 1px"; // Reset border color if input is valid
+                }
+            }
+        });
+    }
 
-document.getElementById("submitBtn").addEventListener("click", submit);
+    document.getElementById("submitBtn").addEventListener("click", submit);
+});
